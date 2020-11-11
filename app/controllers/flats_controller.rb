badf9452d4fd2d411442eddc1b4b3590320b23c4
fannyibanez <fannyibanez89@gmail.com>
2020-11-11 17:05:06 +0100

    rails simple airbnb first try

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,21 @@
+class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Romance in paris',
+  address: '10 rue des fleurs',
+  description: 'hdjkhuizhxbnnnnxnjkjizohefie zoiezhfiezhfozhnslaoidopazdyk hidohuhuhuhiohuhh',
+  price_per_night: 189,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Country house',
+  address: 'Moselle',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 230,
+  number_of_guests: 8
+)
+
+Flat.create!(
+  name: 'Loft in brooklyn',
+  address: '22 street Brooklyn',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 360,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Sky chalet',
+  address: 'La plagne',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 200,
+  number_of_guests: 3
+)
